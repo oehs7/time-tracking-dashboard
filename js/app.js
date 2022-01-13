16545610df5c9162e96c7cd9f49feab251eff124
Omar Hamad <17035664+oehs7@users.noreply.github.com>
2022-01-13 13:55:06 +0100

    Add    console.log(regularCards); to the line 33

--- js/app.js
+++ js/app.js
@@ -30,7 +30,8 @@ fetch('./js/data.json')
 
         //  create  reference to created cards
         regularCards = document.querySelectorAll('.regular-card');
-        
+        console.log(regularCards);
+    
 
 
 // the Functions
